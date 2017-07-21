8bc56ef68ef3ff91a21f3a9a7ac0d92f0a2acf90
Matt Crandell <mrcrandell@gmail.com>
2017-07-20 23:28:40 -0400

    Updated redirects.

--- src/middleware.php
+++ src/middleware.php
@@ -3,9 +3,3 @@
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
 use Psr7Middlewares\Middleware;
-
-$app->add(
-    Middleware::Https(true)   //(optional) True to force https, false to force http (true by default)
-        ->maxAge(1000000)     //(optional) max-age directive for the Strict-Transport-Security header. By default is 31536000 (1 year)
-        ->includeSubdomains() //(optional) To add the "includeSubDomains" attribute to the Strict-Transport-Security header.
-    );
