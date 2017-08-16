53326e8e2808c70dd5bc24f6079fa13de662f7f2
Matt Crandell <mrcrandell@gmail.com>
2017-08-16 06:34:39 -0400

    Updated services and about.

--- views/index.php
+++ views/index.php
@@ -101,7 +101,7 @@
                             <div class="card" data-aos="zoom-in">
                                 <h3 class="card-header">Dentures</h3>
                                 <div class="card-block">
-                                    <p>Description needed.</p>
+                                    <p>Complete(top and bottom) Dentures and we also make Partial Dentures.</p>
                                 </div>
                             </div>
                         </div>
@@ -109,7 +109,7 @@
                             <div class="card" data-aos="zoom-in">
                                 <h3 class="card-header">Ultrasonic Cleaning and Hygiene</h3>
                                 <div class="card-block">
-                                    <p>Ultrasonic dental cleaning is available to remove more dental tartar in less time, with no metal scraping!</p>
+                                    <p>Removes calculus, plaque biofilms, and bacterial byproducts from tooth surfaces.</p>
                                 </div>
                             </div>
                         </div>
@@ -261,13 +261,13 @@
                 <div class="content">
                     <h2>About Us</h2>
                     <h3>Dr. Aaron C. Ford</h3>
-                    <p>Dr. Ford grew up in Sturgis, Michigan and earned his bachelors degree from Western Michigan University in 1999.  While at Western he was involved in the Army ROTC program.  After graduation, Dr. Ford was commissioned in the United States Army as a Second Lieutenant.  He served two years at Fort Hood, Texas in the 1st Calvary Division where he was a medical platoon leader in the Medical Service Corps.  His next assignment took him to Walter Reed Army Medical Center in Washington, DC, where he spent the next two years in the Health Physics Department.</p>
+                    <p>Dr. Ford grew up in Sturgis, Michigan and earned his bachelors degree from Western Michigan University in 1999.  While at Western, he was involved in the Army ROTC program.  After graduation, Dr. Ford was commissioned in the United States Army as a Second Lieutenant.  He served two years at Fort Hood, Texas in the 1st Calvary Division where he was a medical platoon leader in the Medical Service Corps.  His next assignment took him to Walter Reed Army Medical Center in Washington, DC, where he spent the next two years in the Health Physics Department.</p>
                     <p>After his commitment with the Army, Dr. Ford entered dental school in 2003 at the University of Michigan and graduated in 2007.  He moved to Vicksburg in 2009 and really enjoys living in south west Michigan.  Dr. Ford is married to Maureen McGlinchey Ford and they have three children.</p>
                     <p>He is a member of the American Dental Association, Michigan Dental Association, and Kalamazoo Valley District Dental Society.  He participates in many continuing education courses throughout the year.</p>
 
                     <h3>Office Staff</h3>
                     <p>The office is managed by Kris McGlothlen. The front desk is ran by Kris Swope. She has been part of the practice for over 30 years and has the answers to your questions. She is joined by Brenda and Dawn, who will greet you with a smile and are very willing and able to help you with your insurance or billing questions.</p>
-                    <p>Dianne, Jody, and Lori are Dr. Ford's assistants. They bring over 10 years combined experience to the practice. They are very good at making you feel comfortable during procedures and always have warm friendly smiles.</p>
+                    <p>Dianne, Jody, and Lori are Dr. Ford's assistants. They bring over 50 years combined experience to the practice. They are very good at making you feel comfortable during procedures and always have warm friendly smiles.</p>
                     <p>Dana, Heidi, Jen, MaryAnn, Linda, and Jamie are the hygenists.  They all do an excellent job cleaning your teeth while keeping you comfortable.  They are a wealth of knowledge concerning your oral hygiene needs.  Be sure to ask them anything that may be on your mind concerning your oral hygiene.</p>
                 </div>
             </section>
