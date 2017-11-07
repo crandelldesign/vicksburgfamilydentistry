2d1b8b599684ac4eda1e2ea7dee9cc97e64282b1
Matt Crandell <mrcrandell@gmail.com>
2017-11-06 22:57:16 -0500

    Added sitemap, robots, and ga code to site.

--- views/index.php
+++ views/index.php
@@ -42,6 +42,16 @@
                 }
             </style>
         </noscript>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-16197792-12"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-16197792-12');
+        </script>
+
     </head>
     <body>
         <div class="page">
